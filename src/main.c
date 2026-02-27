9b7f975fb5c98343933c298fccd3687faf4740fb
Gus Ballman <gus.ballman@wsu.edu>
2026-02-26 19:23:42 -0800

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,10 +1,5 @@
 #include "../include/rsa.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <unistd.h>
-
 typedef enum {
 CREATE_KEY,
 ENCRYPt_FILE,
@@ -14,9 +9,9 @@ HELP
 
 void displayHelp(void) {
   printf("Kevlar Encryption:\n");
-  printf("\tCreate Keys\t|\t-c <key-names>\n");
+  printf("\tCreate Keys\t|\t-c <key-names> <password>\n");
   printf("\tEncrypt File\t|\t-e <plain-text> <public-key> <outfile-name>\n");
-  printf("\tDecrypt File\t|\t-d <encrypted-file> <private-key> <outfile-name>\n");
+  printf("\tDecrypt File\t|\t-d <encrypted-file> <private-key> <outfile-name> <password>\n");
   printf("\tHelp\t\t|\t-h Prints This Message\n");
 }
 
@@ -28,16 +23,17 @@ int main(int argc, char* argv[]) {
   srand(time(NULL)); 
 
   int opt;
+
   while ((opt = getopt(argc, argv, "cedh")) != -1) {
     switch(opt) {
       case 'c':
-        createKeys(argv[2]);
+        createKeys(argv[2], argv[3]);
         break;
       case 'e':
         encryptFile(argv[2], argv[3], argv[4]);
         break;
       case 'd':
-        decryptFile(argv[2], argv[3], argv[4]);
+        decryptFile(argv[2], argv[3], argv[4], argv[5]);
         break;
       case 'h':
         displayHelp();
