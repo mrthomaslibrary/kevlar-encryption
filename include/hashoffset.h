4270cf6eff6c432e7731df4312c51dd498e4ed54
Gus Ballman <gus.ballman@wsu.edu>
2026-02-27 11:12:04 -0800

    Update hashoffset.h

--- include/hashoffset.h
+++ include/hashoffset.h
@@ -9,10 +9,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef enum rotate {
+RIGHT = 0, LEFT = 1
+} Rotate;
+
 uint32_t nthPrime(int n);
 bool isPrime(int n);
 char* hashPassword(char* password);
 uint32_t hashToPrime(char* hash);
 uint32_t offsetCreation(char* password);
+uint32_t rotate(uint32_t num, uint32_t amount, Rotate direction);
 
 #endif
